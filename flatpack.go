01d6e88c7baf209e00fe0de01b881fd793ea4ba7
Tony Spataro <tony@rightscale.com>
2015-10-26 00:45:33 -0700

    Complete prototype.

--- flatpack.go
+++ flatpack.go
@@ -1,8 +1,10 @@
 package flatpack
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 // Getter represents a read-only repository of key/value pairs where the keys
@@ -15,29 +17,35 @@ type Getter interface {
 	Get(name []string) (string, error)
 }
 
-func Unmarshal(data Getter, v interface{}) error {
-	return unmarshal(data, []string{}, v)
+// Unmarshal reads configuration data into a struct.
+func Unmarshal(data Getter, dest interface{}) error {
+	return unmarshal(data, []string{}, dest)
 }
 
-func unmarshal(data Getter, prefix []string, v interface{}) error {
-	vt := reflect.TypeOf(v)
-	vv := reflect.ValueOf(v)
+func unmarshal(data Getter, prefix []string, dest interface{}) error {
+	v := reflect.ValueOf(dest)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	} else {
+		return fmt.Errorf("Cannot unmarshal: need pointer to struct, got %s", v.Kind().String())
+	}
 
-	// prepare field-name that we can reuse across fields
-	name := make([]string, len(prefix) + 1)
-	copy(name, prefix)
+	vt := v.Type()
 
 	if vt.Kind() != reflect.Struct {
 		return fmt.Errorf("Invalid kind for %v: expected struct, got %s", prefix, vt.Kind().String())
 	}
 
+	// prepare field-name that we can reuse across fields
+	name := make([]string, len(prefix)+1)
+	copy(name, prefix)
+
 	for i := 0; i < vt.NumField(); i++ {
 		field := vt.Field(i)
-		value := vv.Field(i)
+		value := v.Field(i)
 
-		name[len(name) - 1] = field.Name
-		// TODO indirect value if it's not a pointer?
-		err := read(data, name, field.Type, value)
+		name[len(name)-1] = field.Name
+		err := read(data, name, value)
 		if err != nil {
 			return err
 		}
@@ -46,13 +54,77 @@ func unmarshal(data Getter, prefix []string, v interface{}) error {
 	return nil
 }
 
-func read(data Getter, name []string, typ reflect.Type, value reflect.Value) error {
-	switch typ.Kind() {
-	case reflect.Struct:
-		unmarshal(data, name, value.Interface())
+func assign(dest reflect.Value, source string) (err error) {
+	kind := dest.Type().Kind()
+
+	switch kind {
+	case reflect.Bool:
+		boolean, err := strconv.ParseBool(source)
+		if err != nil {
+			dest.SetBool(boolean)
+		}
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		number, err := strconv.ParseInt(source, 10, int(dest.Type().Size()*8))
+		if err == nil {
+			dest.SetInt(number)
+		}
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32,
+		reflect.Uint64, reflect.Uintptr:
+		number, err := strconv.ParseUint(source, 10, int(dest.Type().Size()*8))
+		if err == nil {
+			dest.SetUint(number)
+		}
+	case reflect.Float32, reflect.Float64:
+		number, err := strconv.ParseFloat(source, int(dest.Type().Size()*8))
+		if err == nil {
+			dest.SetFloat(number)
+		}
+	case reflect.String:
+		if err == nil {
+			dest.SetString(source)
+		}
 	default:
-		fmt.Println("Reading", name, "which is a", typ.Kind().String())
+		err = fmt.Errorf("Cannot assign %v to a %s", source, dest.Type().Name())
 	}
 
-	return nil
+	return
+}
+
+// Unmarshal a single field by reading a string from the Getter, massaging it
+// to the correct Type for that field, and assigning to the given Value.
+func read(data Getter, name []string, value reflect.Value) error {
+	kind := value.Type().Kind()
+
+	var err error
+
+	switch kind {
+	case reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32,
+		reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64, reflect.Float32, reflect.Float64,
+		reflect.String:
+		got, err := data.Get(name)
+		if err == nil {
+			err = assign(value, got)
+		}
+	case reflect.Array, reflect.Slice:
+		got, err := data.Get(name)
+		if err == nil {
+			var raw []interface{}
+			err = json.Unmarshal([]byte(got), &raw)
+			if err == nil {
+				value.Set(reflect.MakeSlice(value.Type(), len(raw), len(raw)))
+				for i, elem := range raw {
+					if err == nil {
+						err = assign(value.Index(i), fmt.Sprintf("%v", elem))
+					}
+				}
+			}
+		}
+	case reflect.Struct:
+		unmarshal(data, name, value.Addr().Interface())
+	default:
+		fmt.Println("Skipping", name)
+	}
+
+	return err
 }
--- unmarshal_test.go
+++ unmarshal_test.go
@@ -16,42 +16,52 @@ type family struct {
 }
 
 type person struct {
-	Email  string
-	Age    int
-	Family family
+	Email   string
+	Age     int
+	Family  family
+	Numbers []int
 }
 
 type stubGetter map[string]string
 
 func (s stubGetter) Get(name []string) (string, error) {
 	fullName := ""
-	for _, v := range(name) {
+	for _, v := range name {
 		sep := "_"
-		if fullName == "" { sep = "" }
+		if fullName == "" {
+			sep = ""
+		}
 		fullName = fmt.Sprintf("%s%s%s", fullName, sep, strings.ToUpper(v))
 	}
-	v, _ := s[fullName]
-	return v, nil
+	v, ok := s[fullName]
+	if ok {
+		return v, nil
+	} else {
+		panic(fmt.Errorf("Test failed: code tried to read unexpected key %v", fullName))
+	}
 }
 
 var _ = Describe("Unmarshal", func() {
 	It("works", func() {
 		getter := stubGetter{
-			"EMAIL": "carol@example.com",
-			"AGE": "37",
-			"FAMILY_MOTHER": "Alice",
-			"FAMILY_FATHER": "Bob",
+			"EMAIL":           "carol@example.com",
+			"AGE":             "37",
+			"FAMILY_MOTHER":   "Alice",
+			"FAMILY_FATHER":   "Bob",
 			"FAMILY_SIBLINGS": "[\"Dave\", \"Eve\"]",
+			"NUMBERS":         "[3,7,11,42,76]",
 		}
 
 		expected := person{
 			Email: "carol@example.com",
-			Age: 37,
-			Family: family{Mother: "Alice", Father: "Bob", Siblings: []string{"Dave", "Eve"}},
+			Age:   37,
+			Family: family{Mother: "Alice", Father: "Bob",
+				Siblings: []string{"Dave", "Eve"}},
+			Numbers: []int{3, 7, 11, 42, 76},
 		}
 
 		got := person{}
-		Expect(flatpack.Unmarshal(getter, got)).To(BeNil())
+		Expect(flatpack.Unmarshal(getter, &got)).To(BeNil())
 
 		Expect(got).To(Equal(expected))
 	})
